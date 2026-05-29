e89375ded6dfd2425eaa2528cdcfd810921ae1ee
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-05-29 16:12:15 +0500

    feat: enhance Info and InfoCounter components with improved layout and additional buttons

--- src/Components/Layout/Info.tsx
+++ src/Components/Layout/Info.tsx
@@ -6,17 +6,39 @@ import { InfoCounterProps } from "@/@Types/InfoCounter";
 
 function Info() {
   return (
-    <div className="w-full flex items-center justify-center flex-col gap-10 py-10 px-5">
-      <div className="w-full flex items-center justify-center flex-col gap-10">
-        <img src="./info-mobile.png" alt="" />
-        <Heading
-          heading="Good Life Begins Width A Good Company"
-          subHeading="Ultricies purus dolor viverra mi laoreet at cursus justo. Ultrices purus diam egestas amet faucibus tempor blandit. Elit velit mauris aliquam est diam. Leo sagittis consectetur diam morb"
-          align="text-center lg:text-left"
-        />
-        <Button bg={true} content="Search Job" link="/" width="w-full" />
+    <div className="w-full flex items-center justify-center flex-col gap-10 lg:gap-21.5 py-10 px-5 lg:py-30 lg:px-18">
+      <div className="w-full flex items-center justify-center gap-21.5 flex-col lg:flex-row">
+        {/* Image Div */}
+        <div className="w-[45%] rounded-3xl overflow-hidden flex items-center justify-start">
+          <img src="./info-mobile.png" alt="" />
+        </div>
+        {/* Heading and Buttons */}
+        <div className="w-[70%] flex items-center justify-center flex-col gap-15">
+          <Heading
+            heading="Good Life Begins With A Good Company"
+            subHeading="Ultricies purus dolor viverra mi laoreet at cursus justo. Ultrices purus diam egestas amet faucibus tempor blandit. Elit velit mauris aliquam est diam. Leo sagittis consectetur diam morbi erat aenean. Vulputate praesent congue faucibus in euismod feugiat euismod volutpat"
+            align="text-center lg:text-left"
+          />
+          <div className="w-full flex items-center justify-start gap-6">
+            <Button
+              bg={true}
+              content="Search Job"
+              link="/"
+              width="w-full lg:w-fit"
+            />
+            <Button
+              bg={false}
+              content="Learn more"
+              link="/"
+              display="hidden lg:flex"
+              underline="underline"
+              color="text-primary"
+            />
+          </div>
+        </div>
       </div>
-      <div className="w-full flex items-center justify-center flex-col gap-6">
+      {/* Counter */}
+      <div className="w-full flex items-center justify-between flex-col gap-6 lg:flex-row">
         {INFO_CARDS.map((item: InfoCounterProps, index: number) => (
           <InfoCounter data={item} key={index} />
         ))}
--- src/Components/Layout/InfoCounter.tsx
+++ src/Components/Layout/InfoCounter.tsx
@@ -3,13 +3,14 @@ import { InfoCounterCardsProps } from "@/@Types/InfoCounter";
 
 function InfoCounter({ data }: InfoCounterCardsProps) {
   return (
-    <div className="flex items-center justify-center flex-col gap-7">
+    <div className="flex items-center lg:items-start justify-evenly flex-col gap-7 w-75">
       <h3 className="text-primary font-bold text-6xl text-center">
         {data.count}
       </h3>
       <Heading
         heading={data.heading}
         subHeading={data.paragraph}
+        align="text-center lg:text-left"
         headingSize="text-2xl"
         headingWeight="font-[600]"
       />
