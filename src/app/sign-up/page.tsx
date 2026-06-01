eda5f41c4ae9fa91ffd2f4d1b621ea44fdec239a
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-06-01 15:53:19 +0500

    feat: add sign-up page with form fields and password visibility toggle

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { useState } from "react";
+import Button from "@/Components/Ui/Button";
+import { LuEye, LuEyeOff } from "react-icons/lu";
+
+function Page() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConformPassword, setShowConformPassword] = useState(false);
+
+  return (
+    <div className="w-full h-screen flex">
+      {/* LEFT FORM */}
+      <div className="lg:w-1/2 w-full h-full flex items-center justify-center lg:bg-none bg-[linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url('/auth.jpg')] bg-cover bg-center">
+        <div className="w-[70%] max-w-md flex flex-col gap-6">
+          {/* Heading */}
+          <div className="flex flex-col gap-2">
+            <h2 className="text-4xl font-semibold text-white lg:text-black">
+              Create Account
+            </h2>
+            <p className="text-sm lg:text-gray-600 flex items-center gap-2 text-white">
+              Already have account?
+              <Button
+                bg={false}
+                content="Log in"
+                color="text-primary"
+                underline="underline"
+                link="/login"
+                padding="p-0"
+              />
+            </p>
+          </div>
+
+          {/* FORM */}
+          <form className="flex flex-col gap-4">
+            {/* Full Name */}
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="border border-gray-300 rounded-lg px-4 py-3 outline-none focus:border-primary text-white lg:text-black placeholder:text-gray-500"
+            />
+            {/* Full Name */}
+            <input
+              type="text"
+              placeholder="Username"
+              className="border border-gray-300 rounded-lg px-4 py-3 outline-none focus:border-primary text-white lg:text-black placeholder:text-gray-500"
+            />
+            {/* Email */}
+            <input
+              type="email"
+              placeholder="Email address"
+              className="border border-gray-300 rounded-lg px-4 py-3 outline-none focus:border-primary text-white lg:text-black placeholder:text-gray-500"
+            />
+
+            {/* Password */}
+            <div className="flex items-center border border-gray-300 rounded-lg px-4 py-3 focus-within:border-primary">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="w-full outline-none text-white lg:text-black placeholder:text-gray-500"
+              />
+
+              {showPassword ? (
+                <LuEyeOff
+                  className="text-gray-500 cursor-pointer text-xl"
+                  onClick={() => setShowPassword(false)}
+                />
+              ) : (
+                <LuEye
+                  className="text-gray-500 cursor-pointer text-xl"
+                  onClick={() => setShowPassword(true)}
+                />
+              )}
+            </div>
+            {/* Conform Password */}
+            <div className="flex items-center border border-gray-300 rounded-lg px-4 py-3 focus-within:border-primary">
+              <input
+                type={showConformPassword ? "text" : "password"}
+                placeholder="Conform Password"
+                className="w-full outline-none text-white lg:text-black placeholder:text-gray-500"
+              />
+
+              {showConformPassword ? (
+                <LuEyeOff
+                  className="text-gray-500 cursor-pointer text-xl"
+                  onClick={() => setShowConformPassword(false)}
+                />
+              ) : (
+                <LuEye
+                  className="text-gray-500 cursor-pointer text-xl"
+                  onClick={() => setShowConformPassword(true)}
+                />
+              )}
+            </div>
+            {/* Submit */}
+            <Button
+              bg={true}
+              content="Create Account"
+              link="/"
+              width="w-full"
+            />
+          </form>
+        </div>
+      </div>
+
+      {/* RIGHT IMAGE */}
+      <div className="w-1/2 hidden h-full bg-[linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url('/auth.jpg')] bg-cover bg-center lg:flex items-center justify-center px-10">
+        <h1 className="text-white font-semibold text-4xl leading-tight text-center">
+          Over 1,75,324 candidates <br /> waiting for good employees.
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
