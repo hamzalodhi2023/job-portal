70c80f12b974021be54102bfc316c889f677bb96
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-06-08 11:49:57 +0500

    feat: enhance Testimonials and TestimonialsCards components with layout and content updates

--- src/Components/Layout/Testimonials.tsx
+++ src/Components/Layout/Testimonials.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import Heading from "../Ui/Heading";
+import TestimonialsCards from "./TestimonialsCards";
 
 function Testimonials() {
   return (
-    <div className="w-full h-full bg-primary/10 py-15 px-5">
+    <div className="w-full h-full bg-primary/10 py-15 px-5 gap-15">
       <Heading
         heading="Testimonials from Our Cuctomers"
-        subHeading="At eu lobortis pretium tincidunt amet lacus ut aenean aliquet. Blandit a massa elementum id scelerisque rhoncus. Lectus dolor blandit massa pretium id ultrices phasellus tortor"
+        subHeading="At eu lobortis pretium tincidunt amet lacus ut aenean aliquet. Blandit a massa elementum id"
       />
+      <div className="w-full p-5 flex items-center justify-center flex-col gap-5 lg:flex-row lg:flex-wrap">
+        <TestimonialsCards />
+        <TestimonialsCards />
+      </div>
     </div>
   );
 }
--- src/Components/Layout/TestimonialsCards.tsx
+++ src/Components/Layout/TestimonialsCards.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { IoIosStar } from "react-icons/io";
+import Heading from "../Ui/Heading";
+import Image from "next/image";
+import Avatar from "@/public/Avatar.png";
 
 function TestimonialsCards() {
-  return <div>TestimonialsCards</div>;
+  return (
+    <div className="w-104 h-100 bg-white rounded-[20px] p-10 flex items-center justify-between flex-col shadow-[0_6px_20px_rgba(48,150,137,0.25)]">
+      <div className="flex items-start justify-center flex-col gap-6">
+        <div className="flex items-center justify-start gap-2">
+          <IoIosStar className="text-yellow-500" />
+          <IoIosStar className="text-yellow-500" />
+          <IoIosStar className="text-yellow-500" />
+          <IoIosStar className="text-yellow-500" />
+          <IoIosStar className="text-yellow-500" />
+        </div>
+        <Heading
+          heading="Amazing services"
+          subHeading="Metus faucibus sed turpis lectus feugiat tincidunt. Rhoncus sed tristique in dolor. Mus etiam et vestibulum venenatis"
+          align="text-left"
+          headingSize="text-[24px]"
+          subHeadingSize="text-[16px]"
+          headingWeight="font-[600]"
+          subHeadingWeight="font-[400]"
+        />
+      </div>
+      <div className="w-full flex items-center justify-start gap-4">
+        <Image src="/Avatar.png" width={50} height={50} alt="Avatar" />
+        <div className="flex items-start justify-center flex-col">
+          <h3 className="text-[16px] font-semibold text-left">Marco Kihn</h3>
+          <p className="text-[16px] text-left font-normal text-gray-500">
+            Happy Client
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TestimonialsCards;
