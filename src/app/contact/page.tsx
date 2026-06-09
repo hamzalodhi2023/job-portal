a07f4abbaab3929a7dc09b05253b5930c1f1d887
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-06-09 12:00:39 +0500

    feat: update map iframe source and integrate Footer component into contact page layout

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
 import ContactHeader from "@/Components/Layout/ContactHeader"
 import ContactForm from "@/Components/Layout/ContactForm"
+import Footer from "@/Components/Layout/Footer"
 
 function page() {
   return (
     <div className="h-full">
         <ContactHeader/>
         <ContactForm/>
+        <Footer/>
     </div>
   )
 }
