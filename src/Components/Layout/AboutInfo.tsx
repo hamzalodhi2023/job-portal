9ba5a6be1545ab01de55ccf47db2c5e1c86d9f64
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-06-11 10:50:07 +0500

    feat: improve layout and styling of AboutInfo component for better responsiveness

--- src/Components/Layout/AboutInfo.tsx
+++ src/Components/Layout/AboutInfo.tsx
@@ -1,9 +1,11 @@
 function AboutInfo() {
   return (
     <div className="w-full py-15 flex items-center justify-center flex-col">
-      <div className="w-full px-18 py-10 flex items-center justify-between gap-10">
-        <h2 className="w-1/2 text-[40px] font-semibold">Et nunc ut tempus duis nisl sed massa</h2>
-        <p className="w-1/2 text-[16px] font-normal text-justify">
+      <div className="w-full lg:px-18 px-5 py-10 flex items-center justify-between gap-10 flex-col lg:flex-row">
+        <h2 className="lg:w-1/2 w-full text-[40px] text-center lg:text-left font-semibold">
+          Et nunc ut tempus duis nisl sed massa
+        </h2>
+        <p className="lg:w-1/2 w-full text-[16px] font-normal text-center lg:text-justify">
           Nunc sed a nisl purus. Nibh dis faucibus proin lacus tristique. Sit
           congue non vitae odio sit erat in. Felis eu ultrices a sed massa.
           Commodo fringilla sed tempor risus laoreet ultricies ipsum. Habitasse
@@ -11,7 +13,7 @@ function AboutInfo() {
           quis viverra viverra odio mauris nunc.
         </p>
       </div>
-      <img src="./AboutInfo.png" alt="" className="w-full px-18" />
+      <img src="./AboutInfo.png" alt="" className="w-full px-5 lg:px-18" />
     </div>
   );
 }
