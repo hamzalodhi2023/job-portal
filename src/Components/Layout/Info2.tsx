f3a67ee93875435a781f2b96fac9f8c0fdbff9b6
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2026-06-11 13:06:07 +0500

    feat: update Info2 component layout and improve icon card styling for better responsiveness

--- src/Components/Layout/Info2.tsx
+++ src/Components/Layout/Info2.tsx
@@ -3,7 +3,7 @@ import { LuCrown, LuFileUser } from "react-icons/lu";
 
 function Info2() {
   return (
-    <div className="w-full py-15 px-18 flex lg:flex-row items-center justify-center flex-col gap-16">
+    <div className="w-full py-15 px-5 lg:px-18 flex lg:flex-row items-center justify-center flex-col gap-16">
       <div className="w-full lg:w-1/2 grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 gap-4 h-auto lg:h-125">
         {/* Left Image */}
         <div className="lg:row-span-2 h-60 lg:h-auto">
@@ -40,25 +40,25 @@ function Info2() {
           align="lg:text-left text-center"
         />
         {/* icon cards */}
-        <div className="w-full flex items-center justify-start gap-10">
+        <div className="w-full flex flex-col lg:flex-row items-center justify-start gap-10">
           {/* left div */}
           <div className="flex items-center justify-center flex-col gap-8">
-            <div className="w-full flex items-center justify-start gap-4 px-7.75 py-5.75">
+            <div className="w-full flex-col lg:flex-row flex items-center justify-start gap-4 px-7.75 py-5.75 text-center">
               <LuCrown className="text-primary text-2xl" />
               <p> Quality Job</p>
             </div>
-            <div className="w-full flex items-center justify-start gap-4 px-7.75 py-5.75">
+            <div className="w-full flex-col lg:flex-row flex items-center justify-start gap-4 px-7.75 py-5.75 text-center">
               <LuCrown className="text-primary text-2xl" />
               <p> Top Companies</p>
             </div>
           </div>
           {/* Right div */}
           <div className="flex items-center justify-center flex-col gap-8">
-            <div className="w-full flex items-center justify-start gap-4 px-7.75 py-5.75">
+            <div className="w-full flex-col lg:flex-row flex items-center justify-start gap-4 px-7.75 py-5.75 text-center">
               <LuFileUser className="text-primary text-2xl" />
               <p>Resume builder</p>
             </div>
-            <div className="w-full flex items-center justify-start gap-4 px-7.75 py-5.75">
+            <div className="w-full flex-col lg:flex-row flex items-center justify-start gap-4 px-7.75 py-5.75 text-center">
               <LuCrown className="text-primary text-2xl" />
               <p>Top Talents</p>
             </div>
